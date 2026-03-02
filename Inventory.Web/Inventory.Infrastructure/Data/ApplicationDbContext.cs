83bd9f0d525dda26b1c1d5c3b14a955fb0089b55
Rifatul <rifatul.ism@gmail.com>
2026-03-03 01:13:46 +0600

    Finish Category Enitity

---
+++ Inventory.Web/Inventory.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Inventory.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
