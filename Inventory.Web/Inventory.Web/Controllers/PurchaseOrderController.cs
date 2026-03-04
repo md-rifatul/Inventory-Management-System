8966b89a173c016c66b0206a0292312a087e3c4d
Rifatul <rifatul.ism@gmail.com>
2026-03-04 22:47:48 +0600

    PurchaseOrder create done

--- Inventory.Web/Inventory.Web/Controllers/PurchaseOrderController.cs
+++ Inventory.Web/Inventory.Web/Controllers/PurchaseOrderController.cs
@@ -1,14 +1,18 @@
 ﻿using Inventory.Application.Interfaces.IServices;
+using Inventory.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Inventory.Web.Controllers
 {
     public class PurchaseOrderController : Controller
     {
         private readonly IPurchaseOrderService _purchaseOrderService;
-        public PurchaseOrderController(IPurchaseOrderService purchaseOrderService)
+        private readonly ISupplierService _supplierService;
+        public PurchaseOrderController(IPurchaseOrderService purchaseOrderService, ISupplierService supplierService)
         {
             _purchaseOrderService = purchaseOrderService;
+            _supplierService = supplierService;
         }
 
         [HttpGet]
@@ -16,6 +20,26 @@ namespace Inventory.Web.Controllers
         {
             var purchaseOrders = _purchaseOrderService.GetAllPurchaseOrders();
             return View(purchaseOrders);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var suppliers = _supplierService.GetAllSuppliers();
+            ViewBag.Suppliers = new SelectList(suppliers, "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(PurchaseOrder purchaseOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                _purchaseOrderService.AddPurchaseOrder(purchaseOrder);
+                return RedirectToAction("Index");
+            }
+            var suppliers = _supplierService.GetAllSuppliers();
+            ViewBag.Suppliers = new SelectList(suppliers, "Id", "Name");
+            return View(suppliers);
+
         }
     }
 }
